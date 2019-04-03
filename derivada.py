2bf36795e21f5c27a2580434d39135ba91b7e1bb
Unknown <jua16073@uvg.edu.gt>
2019-04-02 18:35:21 -0600

    2

--- derivada.py
+++ derivada.py
@@ -3,5 +3,7 @@ import numpy as np
 def cost(theta, X, y):
   m, n = X.shape
   h = np.matmul(X, theta)
-  sq = (h - y) * X
+  sq = (h - y)
+  for x in X:
+    sq[x] = sq[x] * X
   return sq.sum() / m
